b8cc8c26c81cf1d9c8f3bb1d7a7d0231ed48cdbf
Yael128 <erik.mora@pslgroup.com>
2020-07-06 09:59:31 -0500

    set Form component state

--- assets/js/components/Form.js
+++ assets/js/components/Form.js
@@ -3,6 +3,15 @@ import React from 'react';
 class Form extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            id: 0,
+            title: '',
+            description: '',
+            size: '',
+            data: '',
+            format: '',
+            submited: false
+        }
     }
     render() {
         return
