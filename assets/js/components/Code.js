04182935c8ea8e0bd6dc9ef29e7c917e1a878a97
Yael128 <erik.mora@pslgroup.com>
2020-07-06 11:20:55 -0500

    create Code component

---
+++ assets/js/components/Code.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Code extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return
+    }
+}
+
+export default Code;
